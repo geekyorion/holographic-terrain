6d546d17adbe487aebfdc8192e207a6416d4726a
Shashank <shashank.ns67@gmail.com>
2023-01-14 18:00:34 +0530

    lines generated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './style/main.css';
 import terrainVertexShader from './shaders/terrain/vertex.glsl';
 import terrainFragmentShader from './shaders/terrain/fragment.glsl';
 
+// Global
+const round = Math.round;
+
 // Canvas
 const canvas = document.querySelector('.webgl');
 
@@ -68,6 +71,52 @@ controls.enableDamping = true;
 // terrain
 const terrain = {};
 
+// texture
+terrain.texture = {};
+
+terrain.texture.width = 32;
+terrain.texture.height = 128;
+terrain.texture.linesCount = 5;
+
+terrain.texture.canvas = document.createElement('canvas');
+terrain.texture.canvas.width = terrain.texture.width;
+terrain.texture.canvas.height = terrain.texture.height;
+terrain.texture.canvas.style.position = 'fixed';
+terrain.texture.canvas.style.top = 0;
+terrain.texture.canvas.style.left = 0;
+terrain.texture.canvas.style.zIndex = 1;
+document.body.append(terrain.texture.canvas);
+
+terrain.texture.context = terrain.texture.canvas.getContext('2d');
+
+terrain.texture.update = () => {
+    terrain.texture.context.clearRect(0, 0, terrain.texture.width, terrain.texture.height);
+    terrain.texture.context.fillStyle = '#ffffff';
+    
+    // Big line
+    terrain.texture.context.globalAlpha = 1;
+    terrain.texture.context.fillRect(
+        0, 0, terrain.texture.width, round(terrain.texture.height * 0.1)
+    );
+    
+    // small lines
+    terrain.texture.context.globalAlpha = 0.5;
+    const smallLinesCount = terrain.texture.linesCount - 1;
+    for (let i = 0; i < smallLinesCount; i++) {
+        terrain.texture.context.fillRect(
+            0,
+            round((terrain.texture.height - 0.2) / smallLinesCount) * i,
+            terrain.texture.width,
+            round(terrain.texture.height * 0.03)
+        );
+    }
+};
+terrain.texture.update();
+
+terrain.texture.instance = new THREE.CanvasTexture(terrain.texture.canvas);
+terrain.texture.instance.wrapS = THREE.RepeatWrapping;
+terrain.texture.instance.wrapT = THREE.RepeatWrapping;
+
 // geometry
 terrain.geometry = new THREE.PlaneGeometry(1, 1, 600, 600);
 terrain.geometry.rotateX(-Math.PI * 0.5);
@@ -80,11 +129,20 @@ terrain.material = new THREE.ShaderMaterial({
     side: THREE.DoubleSide,
     blending: THREE.AdditiveBlending,
     uniforms: {
-        uElevation: { value: 2 }
-    }
+        uElevation: { value: 2 },
+        uAlphaSteps: { value: 0.95 },
+        uTexture: { value: terrain.texture.instance }
+    },
+    wireframe: false
 });
 
 // debug
+gui.Register({
+    object: terrain.material,
+    property: 'wireframe',
+    type: 'checkbox'
+});
+
 gui.Register({
     object: terrain.material.uniforms.uElevation,
     property: 'value',
@@ -93,7 +151,17 @@ gui.Register({
     min: 0.5,
     max: 5,
     step: 0.001
-})
+});
+
+gui.Register({
+    object: terrain.material.uniforms.uAlphaSteps,
+    property: 'value',
+    type: 'range',
+    label: 'uAlphaSteps',
+    min: 0.001,
+    max: 1.000,
+    step: 0.001
+});
 
 // mesh
 terrain.mesh = new THREE.Mesh(terrain.geometry, terrain.material);
--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -1,10 +1,16 @@
+uniform float uAlphaSteps;
+uniform sampler2D uTexture;
+
 varying float vElevation;
 
 void main() {
+    vec4 textureColor = texture2D(uTexture, vec2(0.0, vElevation * 30.0));
+
     // float elevation = vElevation + 0.5;
 
-    float alpha = mod(vElevation * 10.0, 1.0);
-    alpha = step(0.95, alpha);
+    // float alpha = mod(vElevation * 10.0, 1.0);
+    // alpha = step(uAlphaSteps, alpha);
+    // gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+    gl_FragColor = textureColor;
 }
